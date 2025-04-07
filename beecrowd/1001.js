c0647a279682ed65a68c203568f13437256e32ff
KAINABRUMHABEKOST <kainabrum123@gmail.com>
2025-04-07 19:28:24 -0300

    add - exercicio 16

---
+++ beecrowd/1001.js
@@ -0,0 +1,7 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+ var  A = parseFloat(lines[0]);
+ var  B = parseFloat(lines[1]);
+ var  PROD = (A *3.5) + (B *7.5)/11
+ console.log("PROD = " +PROD)
