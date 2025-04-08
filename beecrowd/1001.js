abf8777a2d5c746739ef1ffda6e1ae4077cfee6d
KAINABRUMHABEKOST <kainabrum123@gmail.com>
2025-04-07 22:02:02 -0300

    beecrowd

--- beecrowd/1001.js
+++ beecrowd/1001.js
@@ -3,5 +3,6 @@ var lines = input.split('\n');
 
  var  A = parseFloat(lines[0]);
  var  B = parseFloat(lines[1]);
- var  PROD = (A *3.5) + (B *7.5)/11
- console.log("PROD = " +PROD)
+ var  C = parseFloat(lines[2]);
+ var  MEDIA = (A *2 + B * 3 + B * 5 )/11
+ console.log("MEDIA = " +MEDIA.toFixed(1));
