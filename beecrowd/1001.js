9416115f236721125516d9d784152ae245a57cd2
KAINABRUMHABEKOST <kainabrum123@gmail.com>
2025-04-23 21:54:11 -0300

    js

--- beecrowd/1001.js
+++ beecrowd/1001.js
@@ -1,8 +1,5 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
- var  A = parseFloat(lines[0]);
- var  B = parseFloat(lines[1]);
- var  C = parseFloat(lines[2]);
- var  MEDIA = (A *2 + B * 3 + B * 5 )/11
- console.log("MEDIA = " +MEDIA.toFixed(1));
+ var numerodoFuncionário = parseInt(lines[0]);
+ var 
